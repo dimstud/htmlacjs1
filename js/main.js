92fdc8d282ebfcaa5f253048173c301687ace8c7
dimstud <dimichageev@gmail.com>
2022-01-16 17:21:07 +0300

    Выполнил задание с функциями

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-alert('Привет!');
+function getRandomIntInclusive(min, max) {
+  if (min > max) {
+    alert('Error min > max');
+    return;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+alert(getRandomIntInclusive(5, 15));
+
+function lengthCheck(string, strMaxLeght) {
+  string = string.length;
+  if (string > strMaxLeght) {
+    return alert('Ошибка: превышено количество символов');
+  }
+}
+lengthCheck('test test', 5);
